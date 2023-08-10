fcd87f5b66a2f24dc6f4c8902e9a16dc597b3d2f
Devran ŞEKER <120737148+devranseker@users.noreply.github.com>
2023-08-10 20:35:14 +0300

    Enum

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Enum;
+
+enum Seasons
+{
+    Summer,//yaz
+    Spring,//ilkbahar
+    Autumun,//sonbahar
+    Winter//kış
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+         //Enum
+        1-> enum, bir grup sabiti (değiştirilemez/salt okunur değişkenler) temsil eden özel bir "sınıftır".
+        2-> belirli bir türdeki sabit değerleri temsil etmek için kullanılır.
+         */
+        Seasons season = Seasons.Winter;
+
+        Console.WriteLine("Season:" + season);
+
+        if (season==Seasons.Winter)
+        {
+            Console.WriteLine("Kış ayındayız.");
+
+        }
+        else
+        {
+            Console.WriteLine("Kış ayında değiliz.");
+        }
+
+        Console.Read();
+    }
+}
+
